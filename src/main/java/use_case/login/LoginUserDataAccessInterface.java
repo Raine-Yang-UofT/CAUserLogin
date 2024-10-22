808d5b494f904b9e3c1ae90f4c3f0350ca6c93c0
Raine-Yang-UofT <raine.yang@mail.utoronto.ca>
2024-10-22 12:57:50 -0400

    fix: track current user in in memory DB

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Update the current logged in user.
+     * @param name the logged in user name
+     */
+    void setCurrentUser(String name);
 }
